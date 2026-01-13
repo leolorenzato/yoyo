2e66312006c49320bbe157a395e2f16a7fbedd22
leolorenzato <lorenzatoleonardo@gmail.com>
2026-01-13 18:06:52 +0100

    Account for the terminal height in the view

--- cmd/view.go
+++ cmd/view.go
@@ -10,94 +10,135 @@ import (
 	"github.com/charmbracelet/x/term"
 )
 
-const minTermWidth int = 80
+const (
+	minTermWidth  int = 80
+	minTermHeight int = 60
+)
 
 func (m Model) View() string {
-	termWidth := getTermWidth()
+	termWidth, termHeight := getTermSizeOrMinSize()
 
-	if termWidth < minTermWidth {
+	if termWidth < minTermWidth || termHeight < minTermHeight {
 		return "terminal size too small"
 	}
 
-	contentWidth := getStyleWidth(m.contentBorderStyle, termWidth)
+	log.Printf("term size %d, %d px", termWidth, termHeight)
 
-	title := m.renderTitle(contentWidth)
-	search := m.renderSearch(contentWidth)
-	menu := m.renderMenu(contentWidth)
-	footer := m.renderFooter(contentWidth)
+	contentMaxWidth := getStyleMaxWidth(m.contentBorderStyle, termWidth)
+	contentMaxHeight := getStyleMaxHeight(m.contentBorderStyle, termHeight)
 
-	var contentText string
+	title := m.renderTitle(contentMaxWidth)
+	search := m.renderSearch(contentMaxWidth)
+	footer := m.renderFooter(contentMaxWidth)
+
+	var menuAvailableHeight int
 	if m.searchEnabled {
-		contentText = lipgloss.JoinVertical(
+		menuAvailableHeight = contentMaxHeight -
+			lipgloss.Height(title) -
+			lipgloss.Height(search) -
+			lipgloss.Height(footer)
+	} else {
+		menuAvailableHeight = contentMaxHeight -
+			lipgloss.Height(title) -
+			lipgloss.Height(footer)
+	}
+
+	menu := m.renderMenu(contentMaxWidth, menuAvailableHeight)
+
+	var mainContent string
+	if m.searchEnabled {
+		mainContent = lipgloss.JoinVertical(
 			lipgloss.Left,
 			title,
-			"",
 			search,
 			menu,
-			"",
-			footer,
 		)
 	} else {
-		contentText = lipgloss.JoinVertical(
+		mainContent = lipgloss.JoinVertical(
 			lipgloss.Left,
 			title,
-			"",
 			menu,
-			"",
-			footer,
 		)
 	}
 
-	content := m.contentBorderStyle.Width(contentWidth).Render(contentText)
+	contentText := lipgloss.Place(
+		contentMaxWidth,
+		contentMaxHeight,
+		lipgloss.Left,
+		lipgloss.Top,
+		lipgloss.JoinVertical(
+			lipgloss.Left,
+			mainContent,
+			lipgloss.Place(
+				contentMaxWidth,
+				contentMaxHeight-lipgloss.Height(mainContent),
+				lipgloss.Left,
+				lipgloss.Bottom,
+				footer,
+			),
+		),
+	)
+
+	content := m.contentBorderStyle.
+		Width(contentMaxWidth).
+		Height(contentMaxHeight).
+		Render(contentText)
 
 	return lipgloss.PlaceHorizontal(
 		termWidth,
 		lipgloss.Center,
 		content,
-	) + "\n"
+	)
 }
 
 func (m Model) renderTitle(contentWidth int) string {
-	titleWidth := getStyleWidth(m.titleStyle, contentWidth)
+	titleWidth := getStyleMaxWidth(m.titleStyle, contentWidth)
 	titleText := truncate(m.title, titleWidth, "...")
 
 	return m.titleStyle.Width(titleWidth).Render(titleText)
 }
 
 func (m Model) renderSearch(contentWidth int) string {
-	searchWidth := getStyleWidth(m.searchStyle, contentWidth)
+	searchWidth := getStyleMaxWidth(m.searchStyle, contentWidth)
 	searchText := truncate("🔍 "+m.search, searchWidth, "...")
 
 	return m.searchStyle.Width(searchWidth).Render(searchText)
 }
 
-func (m Model) renderMenu(contentWidth int) string {
-	menuWidth := getStyleWidth(m.menuStyle, contentWidth)
-	menuText := lipgloss.JoinVertical(lipgloss.Left, m.renderMenuItems(menuWidth)...)
-	menuText = truncate(menuText, menuWidth, "...")
+func (m Model) renderMenu(maxContentWidth int, maxContentHeight int) string {
+	menuMaxWidth := getStyleMaxWidth(m.menuStyle, maxContentWidth)
+	menuMaxHeight := getStyleMaxHeight(m.menuStyle, maxContentHeight)
 
-	return m.menuStyle.Width(menuWidth).Render(menuText)
+	startItemIndex := 0
+	availableHeight := menuMaxHeight
+	var items []string
+	for i, cmd := range m.filteredCmds[startItemIndex:] {
+		item := m.renderMenuItem(cmd, i, menuMaxWidth)
+		itemHeight := lipgloss.Height(item)
+		if availableHeight < itemHeight {
+			break
+		}
+		items = append(items, item)
+		availableHeight -= itemHeight
+	}
+
+	menuText := lipgloss.JoinVertical(lipgloss.Left, items...)
+	menuText = truncate(menuText, menuMaxWidth, "...")
+
+	return m.menuStyle.Width(menuMaxWidth).Height(menuMaxHeight).Render(menuText)
 }
 
-func (m Model) renderMenuItems(menuWidth int) []string {
-	selectedStyle := m.selectedMenuTextStyle
-	normalStyle := m.normalTextMenuStyle
-
-	items := make([]string, len(m.filteredCmds))
-	for i, choice := range m.filteredCmds {
-		line := choice.icon + " " + choice.name
-		line = truncate(line, menuWidth, "...")
-		if i == m.cursor {
-			items[i] = selectedStyle.Render(line)
-		} else {
-			items[i] = normalStyle.Render(line)
-		}
+func (m Model) renderMenuItem(item Cmd, itemIndex int, maxWidth int) string {
+	line := item.icon + " " + item.name
+	line = truncate(line, maxWidth, "...")
+	if itemIndex == m.cursor {
+		return m.selectedMenuTextStyle.Render(line)
 	}
-	return items
+	return m.normalTextMenuStyle.Render(line)
 }
 
 func (m Model) renderFooter(contentWidth int) string {
-	footerWidth := getStyleWidth(m.footerStyle, contentWidth)
+	footerWidth := getStyleMaxWidth(m.footerStyle, contentWidth)
 	footerText := truncate(
 		"• ↑/↓ to navigate • enter to select • ctrl+c to quit",
 		footerWidth,
@@ -107,25 +148,35 @@ func (m Model) renderFooter(contentWidth int) string {
 	return m.footerStyle.Width(footerWidth).Render(footerText)
 }
 
-func getTermWidth() int {
-	termWidth, _, err := term.GetSize(os.Stdout.Fd())
+func getTermSizeOrMinSize() (int, int) {
+	termWidth, termHeight, err := term.GetSize(os.Stdout.Fd())
 	if err != nil {
 		termWidth = minTermWidth
+		termHeight = minTermHeight
 		log.Printf(
-			"failed to get terminal size, use default size of %d px",
+			"failed to get terminal size, use default size of %d, %d px",
 			termWidth,
+			termHeight,
 		)
 	}
 
-	return termWidth
+	return termWidth, termHeight
 }
 
-func getStyleWidth(s lipgloss.Style, outerWidth int) int {
+func getStyleMaxWidth(s lipgloss.Style, outerWidth int) int {
 	return outerWidth -
 		s.GetBorderLeftSize() -
 		s.GetBorderRightSize() -
 		s.GetPaddingLeft() -
 		s.GetPaddingRight()
+}
+
+func getStyleMaxHeight(s lipgloss.Style, outerHeight int) int {
+	return outerHeight -
+		s.GetBorderTopSize() -
+		s.GetBorderBottomSize() -
+		s.GetPaddingTop() -
+		s.GetPaddingBottom()
 }
 
 func truncate(s string, length int, tail string) string {
