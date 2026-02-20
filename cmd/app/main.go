06b3cb7e2ac2d20a3748a4dc179a3822d3add0ee
leolorenzato <lorenzatoleonardo@gmail.com>
2026-02-20 16:49:53 +0100

    Add version, fix flags

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"runtime/debug"
 	"yoyo/internal/app"
 	"yoyo/internal/theme"
 
@@ -16,14 +17,22 @@ import (
 const appName string = "yoyo"
 
 func main() {
-	cfgFile := flag.String("c", "cfg_path", "path to configuration file")
-	debug := flag.Bool("debug", false, "enable debug logging")
+	version := flag.Bool("version", false, "print version information")
+	cfgPath := flag.String("c", "", "path to configuration file (required)")
+	debug := flag.Bool("debug", false, "debug mode")
 	flag.Parse()
+
+	if *version {
+		printVersion()
+		os.Exit(0)
+	}
+
+	requireFlag(cfgPath, "c")
 
 	if *debug {
 		f, err := getLogFile()
 		if err != nil {
-			fmt.Println("failed to setup the logger:", err)
+			fmt.Println("failed to setup the logger: ", err)
 			os.Exit(1)
 		}
 		defer f.Close()
@@ -32,8 +41,8 @@ func main() {
 	}
 
 	var cfg Cfg
-	if _, err := toml.DecodeFile(*cfgFile, &cfg); err != nil {
-		log.Printf("failed to load configuration file %s", *cfgFile)
+	if _, err := toml.DecodeFile(*cfgPath, &cfg); err != nil {
+		log.Printf("failed to load configuration file %s", *cfgPath)
 		os.Exit(1)
 	}
 
@@ -52,4 +61,34 @@ func main() {
 		log.Printf("error: %v", err)
 		os.Exit(1)
 	}
+}
+
+func requireFlag(value *string, name string) {
+	if *value == "" {
+		fmt.Printf("%s is required\n", name)
+		os.Exit(1)
+	}
+}
+
+func printVersion() {
+	version := "unknown"
+	commit := "unknown"
+	buildTime := "unknown"
+	goVersion := "unknown"
+	if info, ok := debug.ReadBuildInfo(); ok {
+		version = info.Main.Version
+		goVersion = info.GoVersion
+		for _, setting := range info.Settings {
+			switch setting.Key {
+			case "vcs.revision":
+				commit = setting.Value
+			case "vcs.time":
+				buildTime = setting.Value
+			}
+		}
+	}
+	fmt.Printf("Version: %s\n", version)
+	fmt.Printf("Commit: %s\n", commit)
+	fmt.Printf("Build time: %s\n", buildTime)
+	fmt.Printf("Go version: %s\n", goVersion)
 }
